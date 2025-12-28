fada8e26d750d95e2720983fa2dbb65f48d0d039
Christian Apolloni <christian.apolloni@gmail.com>
2025-12-28 14:21:44 +0100

    Make homebrew plugin portable to Linux

--- .plugins/homebrew/homebrew.plugin.zsh
+++ .plugins/homebrew/homebrew.plugin.zsh
@@ -1,4 +1,7 @@
 if [[ -s /opt/homebrew/bin/brew ]]; then
     export HOMEBREW_NO_ANALYTICS=1
     eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -s /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    export HOMEBREW_NO_ANALYTICS=1
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
