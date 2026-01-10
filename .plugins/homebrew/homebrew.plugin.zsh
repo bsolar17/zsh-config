72d31273408b87b1f992fac191b35624deec179c
Christian Apolloni <christian.apolloni@gmail.com>
2026-01-10 20:00:51 +0100

    Disable homebrew env hints

--- .plugins/homebrew/homebrew.plugin.zsh
+++ .plugins/homebrew/homebrew.plugin.zsh
@@ -1,7 +1,9 @@
 if [[ -s /opt/homebrew/bin/brew ]]; then
     export HOMEBREW_NO_ANALYTICS=1
+    export HOMEBREW_NO_ENV_HINTS=1
     eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ -s /home/linuxbrew/.linuxbrew/bin/brew ]]; then
     export HOMEBREW_NO_ANALYTICS=1
+    export HOMEBREW_NO_ENV_HINTS=1
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
