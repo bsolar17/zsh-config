60476d60e0e54e89099803cd6abf311637b37f25
Christian Apolloni <christian.apolloni@gmail.com>
2026-06-11 19:16:06 +0200

    Update brew config

--- .plugins/homebrew/homebrew.plugin.zsh
+++ .plugins/homebrew/homebrew.plugin.zsh
@@ -1,10 +1,14 @@
 if [[ -s /opt/homebrew/bin/brew ]]; then
     export HOMEBREW_NO_ANALYTICS=1
     export HOMEBREW_NO_ENV_HINTS=1
+    export HOMEBREW_NO_ASK=1
+    export HOMEBREW_UPGRADE_GREEDY=1
     eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ -s /home/linuxbrew/.linuxbrew/bin/brew ]]; then
     export HOMEBREW_NO_ANALYTICS=1
     export HOMEBREW_NO_ENV_HINTS=1
+    export HOMEBREW_NO_ASK=1
+    export HOMEBREW_UPGRADE_GREEDY=1
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 if [[ -d "$HOMEBREW_PREFIX/opt/ffmpeg-full/bin" ]]; then
