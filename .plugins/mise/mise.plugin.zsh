54bac03372aff053b2d2c464703e1c8800f6470d
Christian Apolloni <christian.apolloni@gmail.com>
2026-08-02 19:39:30 +0200

    Add envmise alias to load .env easily

--- .plugins/mise/mise.plugin.zsh
+++ .plugins/mise/mise.plugin.zsh
@@ -1,4 +1,5 @@
-export MISE_DEFAULT_CONFIG_FILENAME=".mise.toml"
 if command -v mise >/dev/null 2>&1; then
+    export MISE_DEFAULT_CONFIG_FILENAME=".mise.toml"
+    alias envmise="eval '\$(MISE_ENV_FILE=.env mise env)'"
     eval "$(mise activate zsh)"
 fi
